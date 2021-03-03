c092b70f90e4d83d43178219f8d7d14226ce648b
leesagacious <sensor1010@163.com>
2021-03-03 03:26:46 -0800

    Add lf_driver struct
    Signed-off-by: leesagacious <sensor1010@163.com>

--- salve_lf.c
+++ salve_lf.c
@@ -1,5 +1,13 @@
 
-
+static struct i2c_driver fl_driver = {
+	.driver = {
+		.name = "fl_sensor",
+		.pm = &fl_pm_ops,
+		.of_match_table = fl_of_match,
+	},
+	.probe_new = fl_probe,
+	.remove = fl_remove,
+};
 
 module_i2c_driver(fl_driver)
 
