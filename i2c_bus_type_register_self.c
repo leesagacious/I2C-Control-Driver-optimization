d0553e8e08b713187f37949ba9e819609243dddb
leesagacious <sensor1010@163.com>
2021-03-10 04:53:27 -0800

    Add i2c match function.
    Signed-off-by: leesagacious <sensor1010@163.com>

--- i2c_bus_type_register_self.c
+++ i2c_bus_type_register_self.c
@@ -1,3 +1,18 @@
+/*
+ * The match function is a very beautiful function. 
+ * one parameter is device and the other is device driver
+ */
+static int i2c_device_match(struct device *dev, struct device_driver *drv)
+{
+
+
+	return 0;
+}	
+
+struct bus_type i2c_bus_type_self = {
+	.name = "self_i2c_lee",
+	.match = self_appropriate,
+};
 
 static int __init i2c_self_init(void)
 {
